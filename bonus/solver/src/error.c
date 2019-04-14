60ddd90425a7a394de7963f43eb93df23e239557
Joris <joris.bertaigne@epitech.eu>
2019-04-15 01:03:49 +0200

    [Fix] fixing bonus

--- bonus/solver/src/error.c
+++ bonus/solver/src/error.c
@@ -14,11 +14,9 @@ int check_maze_errors(maze_t *maze)
 
     for (; (maze->grid && maze->grid[standard_size]) &&
     maze->grid[standard_size] != '\n'; standard_size++);
-    for (int i = 0; i < (maze->height + 1) * maze->width - 1; i++) {
-        if (maze->grid[i] != 'X' && maze->grid[i] != '*' && maze->grid[i] != '\n')
-            return (84);
-        line_size = (maze->grid[i] == '\n') ? 0 : line_size + 1;
-        if (line_size > standard_size)
+    for (int i = 0; i < ((maze->height + 1) * maze->width) - 1; i++) {
+        if ((maze->grid[i] != 'X' && maze->grid[i] != '*')
+        && (maze->grid[i] != '\n' && maze->grid[i] != '\0'))
             return (84);
     }
     return (0);
